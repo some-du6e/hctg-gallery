f3ac90e8c1d9327044dd56ce2b8153b0a54be77b
some-du6e <103129948+some-du6e@users.noreply.github.com>
2026-05-17 21:03:48 -0600

    add search command to the slack thing

--- components/slack.py
+++ components/slack.py
@@ -1,18 +1,16 @@
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+import components.galleryManagerFrontend as fe
+import components.jsonManager as jm
+from dotenv import load_dotenv
+from slack_bolt import App
+import threading
 import os
 
-from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
-
-import components.galleryManagerFrontend as fe
 
 
-from dotenv import load_dotenv
 load_dotenv()
-# This sample slack application uses SocketMode
-# For the companion getting started setup guide,
-# see: https://docs.slack.dev/tools/bolt-python/getting-started
 
-# Initializes your app with your bot token
+
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 @app.message("wqdninqwio")
@@ -22,15 +20,99 @@ def message_hello(message, say, client):
 
 
 @app.command("/updategallery")
-def handle_slash_command(ack, say, command, logger):
+def update_gallery_command(ack, say, command, logger):
     ack()
     say(f"<@{command['user_id']}> made me update the gallery mirror")
     
-    # Run in a separate thread to avoid Playwright asyncio loop error
-    import threading
+    
     t = threading.Thread(target=fe.updateGalleryJSON, args=(say,))
     t.start()
 
+
+# @app.command("/hellotestf")
+# def testing(ack, say, command, logger):
+#     ack()
+#     say("hi" + f"<@{command['user_id']}>")
+
+@app.command("/hctgprojectinfo")
+def getproject(ack, say, command, logger):
+    ack()
+
+    commandtext = command["text"]
+    try:
+        projectId = int(commandtext)
+    except Exception as e:
+        say("Please provide a valid project ID " + f"<@{command['user_id']}>")
+        return
+
+    try:
+        project = jm.getProjectById(projectId)
+    except Exception as e:
+        say("An error occurred while fetching the project info " + f"<@{command['user_id']}>")
+        return
+    
+    if project is None:
+        say("No project found with that ID " + f"<@{command['user_id']}>")
+        return
+    
+    blocks = [
+		{
+			"type": "markdown",
+			"text": "# PROJECT NAME \n ### by AUTHOR \n ------\n DESCRIPTION \n \n\n :clock5: 54h4m \n :ticket: 21"
+		},
+		{
+			"type": "image",
+			"image_url": "https://placehold.co/840x420.png",
+			"alt_text": "delicious tacos"
+		},
+		{
+			"type": "markdown",
+			"text": "---- \n ## 🤖 AI declaration\n "
+		},
+		{
+			"type": "markdown",
+			"text": "\n hi so i vibe coded everything bc i hate hack club and love doing hackatime f raud "
+		},
+		{
+			"type": "markdown",
+			"text": "----"
+		},
+		{
+			"type": "actions",
+			"block_id": "buttons",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "🔗 Demo"
+					},
+					"url": "https://api.slack.com/block-kit"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": ":github: Repository"
+					},
+					"url": "https://api.slack.com/block-kit"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": ":book: Readme"
+					},
+					"url": "https://api.slack.com/block-kit"
+				}
+			]
+		}
+	]
+    say(text=f"here's the info for project id {projectId} " + f"<@{command['user_id']}>")
+    say(
+        text=f"information about project",
+        blocks=blocks
+    )
 
 # Start your app
 if __name__ == "__main__":
